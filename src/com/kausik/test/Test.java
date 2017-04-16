94d8f93c3c4f9ce6814d1b763343c402e7975904
kausikbhandari <kausik.bhandari@gmail.com>
2017-04-16 11:35:06 +0530

    initial commit
    created core java project

---
+++ src/com/kausik/test/Test.java
@@ -0,0 +1,15 @@
+package com.kausik.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		boolean b = false;
+		if(b==true){
+			System.out.println("True");
+		}else{
+			System.out.println("False");
+		}
+
+	}
+
+}
